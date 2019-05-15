45cd9dc956f673f563bdf1be58123ba16f88389c
Taylor Lyles <hawks93187@gmail.com>
2019-05-15 13:26:00 -0700

    Added View Controller, show segue from bar button, textfield and view in VC. Added nav item and bar button, created a cocoa and connected it. Created outlets for textfield and view and an action for send.

---
+++ MessageBoard/MessageBoard/MessageDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MessageDetailViewController.swift
+//  MessageBoard
+//
+//  Created by Taylor Lyles on 5/15/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+	
+	@IBAction func sendButton(_ sender: Any) {
+	}
+	
+	@IBOutlet weak var textField: UITextField!
+	@IBOutlet weak var textView: UITextView!
+	
+}
