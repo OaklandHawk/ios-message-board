19afdc91ed8b403180a0705b251b6db54ebe9497
Taylor Lyles <hawks93187@gmail.com>
2019-05-15 16:18:22 -0700

    MessageThreadsTVC; implemented rows and cellforrow reloaded table view, completion of closure prepare for segue
    MessageThreadDetailTableViewController
    implemented rows and cellarrowat
    viewdidload
    set vars up
    prepare for segue

--- MessageBoard/MessageBoard/ViewController/MessageThreadsTableViewController.swift
+++ MessageBoard/MessageBoard/ViewController/MessageThreadsTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MessageThreadsTableViewController: UITableViewController {
+	
+	var messageThreadController : MessageThreadController = MessageThreadController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +26,21 @@ class MessageThreadsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		
-        return 0
+        return messageThreadController.messageThreads.count
     }
 
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "BasicCell", for: indexPath)
+		let message = messageThreadController.messageThreads[indexPath.row]
+		cell.textLabel?.text = message.title
 		return cell
+	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "CellSegue" {
+			let destinationVC: MessageThreadDetailTableViewController = segue.destination as! MessageThreadDetailTableViewController
+			guard let index = tableView.indexPathForSelectedRow?.row else { return }
+			destinationVC.messageThreads = messageThreadController.messageThreads[index]
+		}
 	}
 }
