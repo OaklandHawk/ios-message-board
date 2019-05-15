ce1ae080c14f73440a13a65fecc45f4561e1875c
Taylor Lyles <hawks93187@gmail.com>
2019-05-15 13:14:06 -0700

    Added View as head and text field in view. Gave cell identifier and set style to basic. Created Cocoa TableViewController and set class in storyboard. Created outlet and action from textfield and set action's event to "Did End On Exit".

---
+++ MessageBoard/MessageBoard/MessageThreadsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MessageThreadsTableViewController.swift
+//  MessageBoard
+//
+//  Created by Taylor Lyles on 5/15/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		
+    }
+
+	@IBOutlet weak var textFieldOutlet: UITextField!
+	
+	@IBAction func textFieldAction(_ sender: Any) {
+	}
+	
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+        return 0
+    }
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		<#code#>
+	}
+}
